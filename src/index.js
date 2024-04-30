b20a73165d6e77c7d5b2b4d54a59b6382ec43b69
Aamir Majeed Khan <aamirmajeedkhan570@gmail.com>
2024-04-30 19:05:31 +0530

    utils code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 dotenv.config()
-connectDB();
+// ConnectDB is returning a Promise
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 3000,()=>{
+        console.log(`Server is Listening at http://localhost:3000`);
+    } )
+})
+.catch((error)=>{
+    console.log("Failed To Connect DB!!",error)
+})
