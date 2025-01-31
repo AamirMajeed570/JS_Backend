1547de4c01159408d9a05d58d36e7af08d0dcba6
aamirmajeed <127582687+deepaklegitbytes@users.noreply.github.com>
2025-02-01 00:42:32 +0530

    CRUD Operation

--- FormHandlingMongoDb/server/controller/user.controller.js
+++ FormHandlingMongoDb/server/controller/user.controller.js
@@ -18,3 +18,78 @@ export const registerCtrl = async(req,res)=>{
     })
 }
  
+export const getAllUsersCtrl = async (req,res) =>{
+    try {
+        const allUsers = await User.find();
+        if(!allUsers){
+            return res.status(300).send({
+                message: "No Users Found",
+            })
+        }
+
+        return res.status(200).send({
+            message: "Success",
+            data: allUsers,
+        })
+    } catch (error) {
+        res.status(300).send({
+            message: "Something went wrong",
+            error: error.message,
+        })
+    }
+}
+
+export const deleteUserCtrl = async (req,res) =>{
+    try {
+        const {id} = req.params;
+        const findUser = await User.findByIdandDelete(id);
+        if(!findUser){
+            return res.status(300).send({
+                message: "User Not Found",
+            })
+        }
+
+        return res.status(200).send({
+            message: "Success",
+            data: findUser,
+        })
+
+    } catch (error) {
+        return res.status(404).send({message: "Not Found"})
+    }
+}
+
+export const updateUserCtrl = async (req,res) =>{
+    try {
+        const {id} = req.params;
+        const {name, email, password} = req.body;
+
+        if(!name || !email || !password){
+            return res.status(300).send({
+                message: "All Fields are required",
+            })
+        }
+
+        const findUser = await User.findByIdAndUpdate(id,{
+            name: name,
+            email: email,
+            password: password,
+        });
+
+        return res.status(200).send({
+            message: "Success",
+            data: findUser,
+        })
+    } catch (error) {
+        return res.status(404).send({message: "Not Found"})
+    }
+}
+
+
+
+
+// CRUD
+// C- Create
+// R- Read
+// U-Update
+// D- Delete
